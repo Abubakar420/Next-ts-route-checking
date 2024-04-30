e1b044a85a57e52278d99ab218b07295d289fe7f
abubakar <abubakarrehman395@gmail.com>
2024-04-30 04:22:46 -0700

    Testing for routing

---
+++ src/pages/info.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; // Import React if you're using React JSX syntax
+
+// Define a functional component
+const Info: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1>Info</h1>
+      <p>This is a basic TypeScript with JSX setup.</p>
+    </div>
+  );
+};
+
+export default Info; // Export the component
