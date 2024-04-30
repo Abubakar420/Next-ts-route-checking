701379c1437ed9e3d71a90cdab8f8b0cd1d4d86c
abubakar <abubakarrehman395@gmail.com>
2024-04-30 03:38:59 -0700

    First commit

---
+++ src/pages/aboutus.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Define a functional component
+const Aboutus: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1>About Us</h1>
+      <p>This is a basic TypeScript with JSX setup.</p>
+    </div>
+  );
+};
+
+export default Aboutus;
---
+++ src/pages/service.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; // Import React if you're using React JSX syntax
+
+// Define a functional component
+const Aboutus: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1>Service</h1>
+      <p>This is a basic TypeScript with JSX setup.</p>
+    </div>
+  );
+};
+
+export default Aboutus; // Export the component
